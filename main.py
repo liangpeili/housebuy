2343590404ffbf3b7596a5bf583e5212c4372b71
liangpeili <liangpeili@foxmail.com>
2015-12-30 14:09:26 +0800

    version2

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 #coding:utf-8
-#Author:liangpeili
-
 
 from flask import Flask,request
 import hashlib
 import xml.etree.ElementTree as ET
-from query import query_db
+from query import query_db, query_db_price, query_db_area, query_db_all
 import sys
+import re
+
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -43,11 +43,72 @@ def return_district():
     try:
         if mydict['Content'] == 'Hi':
             mydic['Content'] = '你好，我是你的买房私人专家。你想买哪里的房子？'
+        elif re.match(r'^a', mydict['Content']):
+            pattern_a = re.compile(r'\d+')
+            global area
+            area = pattern_a.findall(mydict['Content'])
+            area1 = int(area[0])
+            area2 = int(area[1])
+            print "area1:" + str(area1) + "area2:" + str(area2)
+            try:
+                mydic['Content'] = query_db_area(area1, area2)
+                print mydic['Content']
+                
+            except:
+                print "query db erorr"
+                mydic['Content'] = '数据库查询错误，请重新输入。'
+                
+        elif re.match(r'^p', mydict['Content']):
+            pattern_p = re.compile(r'\d+')
+            global price
+            price = pattern_p.findall(mydict['Content'])
+            price1 = int(price[0])
+            price2 = int(price[1])
+            print "price1:" + price[0] + "price2:" + price[1]
+            try:
+                mydic['Content'] = query_db_price(price1, price2)
+                print mydic['Content']
+                
+            except:
+                print "query db erorr"
+                mydic['Content'] = '数据库查询错误，请重新输入。'
         else:
-            mydic['Content'] = query_db(mydict['Content'])
+            print 'here else'
+            if mydict['Content'] != '合并查询':
+                global addr
+                addr = mydict['Content']
+                try:
+                    mydic['Content'] = query_db(addr)
+                    print mydic['Content']
+                
+                except:
+                    print "query db erorr"
+                    mydic['Content'] = '数据库查询错误，请重新输入。'
+            elif mydict['Content'] == '合并查询':
+                area1 = int(area[0])
+                area2 = int(area[1])      
+                price1 = int(price[0])
+                price2 = int(price[1])
+                print area[0] + area[1] + price[0] + price[1] + addr
+                try:
+                    mydic['Content'] = query_db_all(price1, price2, area1, area2, addr)
+                    print mydic['Content']
+
+                except:
+                    print "query db erorr"
+                    mydic['Content'] = '数据库查询错误，请重新输入。'
+
+                #print 'here he'
+                #print price, area, addr
+            else:
+                mydic['Content'] = '您的输入有误，请重新输入。'
     except:
         mydic['Content'] = '您的输入有误，请重新输入。'
     
+
+    f = open('content.txt', 'a+')
+    f.write(mydict['Content']+'\n')
+    f.close()
     myxml = '''\
     <xml>
     <ToUserName><![CDATA[{}]]></ToUserName>
--- query.py
+++ query.py
@@ -9,14 +9,60 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def query_db(district):
-    conn = MySQLdb.connect(host="localhost",user="house",passwd="xxxxxx",db="housebuy",charset="utf8")
+    conn = MySQLdb.connect(host="localhost",user="root",passwd="Ppnn13fish,",db="housebuy",charset="utf8")
     cur = conn.cursor()
     cur.execute("select * from soufang where district = %s limit 3", (district,))
     lines = cur.fetchall()
     i = 1
+#    for line in lines:
+#        print "第%d条信息：" % i
+#        print '标题:'+line[2]
+#        print '价格:'+line[3]
+#        print '户型:'+line[4]
+#        print '面积:'+line[5]
+#        print '中介:'+line[6]
+#        print '电话:'+line[9]
+#        print '地区:'+line[7]
+#        print '街道:'+line[8]
+#        print '详细介绍:'+line[10]
+#        i += 1
+#        print "-"* 30
+    cur.close()
+    conn.close()
+    return '标题:'+lines[0][2]+'\n'+'价格:'+str(lines[0][3])+'\n'+'户型:'+lines[0][4]+'\n'+'面积:'+str(lines[0][5])+'\n'+'中介:'+lines[0][6]+'\n'+'电话:'+lines[0][9]+'\n'+'地区:'+lines[0][7]+'\n'+'街道:'+lines[0][8]+'\n'+'详细介绍:'+lines[0][10]
 
-    return '标题:'+lines[0][2]+'\n'+'价格:'+lines[0][3]+'\n'+'户型:'+lines[0][4]+'\n'+'面积:'+lines[0][5]+'\n'+'中介:'+lines[0][6]+'\n'+'电话:'+lines[0][9]+'\n'+'地区:'+lines[0][7]+'\n'+'街道:'+lines[0][8]+'\n'+'详细介绍:'+lines[0][10]
+def query_db_area(area1, area2):
+    conn = MySQLdb.connect(host="localhost",user="root",passwd="Ppnn13fish,",db="housebuy",charset="utf8")
+    cur = conn.cursor()
+    cur.execute("select * from soufang where area between %s and %s limit 3", (area1, area2))
+    lines = cur.fetchall()
+    
+    cur.close()
+    conn.close()
+    return '标题:'+lines[0][2]+'\n'+'价格:'+str(lines[0][3])+'\n'+'户型:'+lines[0][4]+'\n'+'面积:'+str(lines[0][5])+'\n'+'中介:'+lines[0][6]+'\n'+'电话:'+lines[0][9]+'\n'+'地区:'+lines[0][7]+'\n'+'街道:'+lines[0][8]+'\n'+'详细介绍:'+lines[0][10]
+
+
+def query_db_price(price1, price2):
+    conn = MySQLdb.connect(host="localhost",user="root",passwd="Ppnn13fish,",db="housebuy",charset="utf8")
+    cur = conn.cursor()
+    cur.execute("select * from soufang where price between %s and %s limit 3", (price1, price2))
+    lines = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return '标题:'+lines[0][2]+'\n'+'价格:'+str(lines[0][3])+'\n'+'户型:'+lines[0][4]+'\n'+'面积:'+str(lines[0][5])+'\n'+'中介:'+lines[0][6]+'\n'+'电话:'+lines[0][9]+'\n'+'地区:'+lines[0][7]+'\n'+'街道:'+lines[0][8]+'\n'+'详细介绍:'+lines[0][10]
+
+def query_db_all(price1, price2, area1, area2, addr):
+    conn = MySQLdb.connect(host="localhost",user="root",passwd="Ppnn13fish,",db="housebuy",charset="utf8")
+    cur = conn.cursor()
+    cur.execute("select * from soufang where price between %s and %s and area between %s and %s and district = %s limit 3", (price1, price2, area1, area2, addr))
+    lines = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return '标题:'+lines[0][2]+'\n'+'价格:'+str(lines[0][3])+'\n'+'户型:'+lines[0][4]+'\n'+'面积:'+str(lines[0][5])+'\n'+'中介:'+lines[0][6]+'\n'+'电话:'+lines[0][9]+'\n'+'地区:'+lines[0][7]+'\n'+'街道:'+lines[0][8]+'\n'+'详细介绍:'+lines[0][10]
 
 if __name__ == "__main__":
     district = raw_input('你想买哪里的房子？')
     query_db(district)
+
